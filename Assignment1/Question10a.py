b3713146637f136459ba79a09dd0045130a62bb9
RealNxiss <nischaymorya@gmail.com>
2023-01-30 18:56:32 +0530

    Finally?

--- Assignment1/Question10a.py
+++ Assignment1/Question10a.py
@@ -1,3 +1,2 @@
-#function takes principal, rate and time as input and returns the compound interest
 def compound_interest(principal, rate, time):
     return principal * (pow((1 + rate / 100), time))
--- Assignment1/Question10b.py
+++ Assignment1/Question10b.py
@@ -3,4 +3,5 @@ import Question10a as q10a
 principal = int(input("Enter the principal amount: "))
 rate = float(input("Enter the rate of interest: "))
 time = int(input("Enter the time period: "))
+print("Principal amount is: ", principal, "Rate of interest is: ", rate, "Time period is: ", time)
 print("Compound interest is: ", q10a.compound_interest(principal, rate, time))
